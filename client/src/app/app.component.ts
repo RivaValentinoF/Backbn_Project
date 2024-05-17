7b881034a91ded50e46543f4a01ff0631a6dce48
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2024-05-17 11:00:49 +0000

    Rispondi alle seguenti domande

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  data = {};
+  constructor(private http: HttpClient)
+  {
+    this.http.get("/simple_json")
+    .subscribe(
+      (data) => 
+      {
+        this.data = data;
+        console.log(data);
+      }
+    )
+  }
 }
