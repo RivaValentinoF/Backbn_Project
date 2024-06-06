b9b362bb3313148f6c885c0c99aee8c84e76c13b
Nabb0 <rivavalentino.f@iisgalvanimi.edu.it>
2024-06-06 11:51:37 +0000

    Inviare i dati al server

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AnimalsService } from './animals.service';
 import { Animal, VettAnimal } from './animals.service';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +10,10 @@ import { Animal, VettAnimal } from './animals.service';
 export class AppComponent {
   title = 'client';
   data = new Array<Animal>();
+  form = new FormGroup({
+    "name": new FormControl(),
+    "type": new FormControl(),
+  });
   //Mi faccio iniettare l'animal servce
   constructor(private animalService : AnimalsService)
   {
@@ -16,5 +21,11 @@ export class AppComponent {
     this.animalService.getAnimals().subscribe(
       (data: VettAnimal)=>{this.data = data['animals']}
     )
+
+  }
+  onSubmit() {
+    console.log("reactive form submitted");
+    console.log(this.form.controls['name'].value);
+    console.log(this.form.controls['type'].value);
   }
 }
